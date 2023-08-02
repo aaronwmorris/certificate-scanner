36f221de116258e760aae7aa04294e69c938b7ee
Aaron W Morris <aaron@aarmor.net>
2023-08-02 17:40:59 -0400

    add ssl scan info

--- expired-certificate-scanner.py
+++ expired-certificate-scanner.py
@@ -3,7 +3,7 @@
 # Author:  Aaron W Morris <aaron@aarmor.net>
 
 
-#import sys
+import sys
 import logging
 import re
 import time
@@ -193,12 +193,22 @@ class expired_certificate_scanner(object):
                 network_configuration=network_configuration,
                 scan_commands=[
                     ScanCommand.CERTIFICATE_INFO,
+                    #ScanCommand.SSL_2_0_CIPHER_SUITES,
+                    #ScanCommand.SSL_3_0_CIPHER_SUITES,
+                    #ScanCommand.TLS_1_0_CIPHER_SUITES,
+                    #ScanCommand.TLS_1_1_CIPHER_SUITES,
+                    #ScanCommand.TLS_1_2_CIPHER_SUITES,
+                    #ScanCommand.TLS_1_3_CIPHER_SUITES,
                 ]
             )
 
 
             scan_list.append(scan_req)
 
+
+        if len(scan_list) == 0:
+            logger.error('All scans complete')
+            sys.exit(1)
 
         logger.info('Queuing scans')
         scanner.queue_scans(scan_list)
@@ -231,8 +241,9 @@ class expired_certificate_scanner(object):
                 continue
 
 
-            certinfo = scan_result.scan_result.certificate_info
-            if certinfo.status == ScanCommandAttemptStatusEnum.ERROR:
+            ### Certificate info ###
+            certificate_info = scan_result.scan_result.certificate_info
+            if certificate_info.status == ScanCommandAttemptStatusEnum.ERROR:
                 logger.error('Error at %s:%d', scan_entry.host, scan_entry.port)
                 #logger.error('Error: %d', scan_result)
 
@@ -243,42 +254,134 @@ class expired_certificate_scanner(object):
                 continue
 
 
-            # Print the Common Names within the certificate chain
-            cert_deployment_0 = certinfo.result.certificate_deployments[0]
-
-            #configured_certificate_chain = [cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
-            #                                for cert in cert_deployment_0.received_certificate_chain]
-            #logger.info('Configured Chain: %s', configured_certificate_chain)
-
-            fingerprint = binascii.hexlify(cert_deployment_0.received_certificate_chain[0].fingerprint(hashes.SHA1())).decode('ascii')
-            #logger.info('Fingerprint: %s', fingerprint)
-
-
-            try:
-                cn = cert_deployment_0.received_certificate_chain[0].subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
-            except IndexError:
-                logger.error('Certificate CN error')
-                cn = 'CN ERROR'
-
-            try:
-                issuer = cert_deployment_0.received_certificate_chain[0].issuer.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
-            except IndexError:
-                logger.error('Certificate issuer error')
-                issuer = 'ISSUER ERROR'
-
-            expire = cert_deployment_0.received_certificate_chain[0].not_valid_after
-
-            scan_entry.cn = cn
-            scan_entry.issuer = issuer
-            scan_entry.expire = expire
-            scan_entry.fingerprint = fingerprint
-
-            if cn == issuer:
-                scan_entry.selfsigned = True
+            self.getCertificateInfo(scan_entry, scan_result)
+            self.getSslInfo(scan_entry, scan_result)
 
             scan_entry.state = ScanState.COMPLETE
-
             self.session.commit()
+
+
+    def getCertificateInfo(self, scan_entry, scan_result):
+        certificate_info = scan_result.scan_result.certificate_info
+
+        # Print the Common Names within the certificate chain
+        cert_deployment_0 = certificate_info.result.certificate_deployments[0]
+
+        #configured_certificate_chain = [cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
+        #                                for cert in cert_deployment_0.received_certificate_chain]
+        #logger.info('Configured Chain: %s', configured_certificate_chain)
+
+        fingerprint = binascii.hexlify(cert_deployment_0.received_certificate_chain[0].fingerprint(hashes.SHA1())).decode('ascii')
+        #logger.info('Fingerprint: %s', fingerprint)
+
+
+        try:
+            cn = cert_deployment_0.received_certificate_chain[0].subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
+        except IndexError:
+            logger.error('Certificate CN error')
+            cn = 'CN ERROR'
+
+        try:
+            issuer = cert_deployment_0.received_certificate_chain[0].issuer.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
+        except IndexError:
+            logger.error('Certificate issuer error')
+            issuer = 'ISSUER ERROR'
+
+        expire = cert_deployment_0.received_certificate_chain[0].not_valid_after
+
+        scan_entry.cn = cn
+        scan_entry.issuer = issuer
+        scan_entry.expire = expire
+        scan_entry.fingerprint = fingerprint
+
+        if cn == issuer:
+            scan_entry.selfsigned = True
+
+        self.session.commit()
+
+
+    def getSslInfo(self, scan_entry, scan_result):
+        ### SSL info ###
+
+        ssl_2_0 = scan_result.scan_result.ssl_2_0_cipher_suites
+        if not isinstance(ssl_2_0.result, type(None)):
+            if len(ssl_2_0.result.accepted_cipher_suites) > 0:
+                ssl_2_0_cipher_list = [x.cipher_suite.name for x in ssl_2_0.result.accepted_cipher_suites]
+                #logger.info('SSL 2: %s', pformat(ssl_2_0_cipher_list))
+                scan_entry.sslv2_0 = ','.join(ssl_2_0_cipher_list)
+            else:
+                #logger.info('SSLv2 disabled')
+                scan_entry.sslv2_0 = 'disabled'
+        else:
+            scan_entry.sslv2_0 = 'no_scan'
+
+
+        ssl_3_0 = scan_result.scan_result.ssl_3_0_cipher_suites
+        if not isinstance(ssl_3_0.result, type(None)):
+            if len(ssl_3_0.result.accepted_cipher_suites) > 0:
+                ssl_3_0_cipher_list = [x.cipher_suite.name for x in ssl_3_0.result.accepted_cipher_suites]
+                #logger.info('SSL 3: %s', pformat(ssl_3_0_cipher_list))
+                scan_entry.sslv3_0 = ','.join(ssl_3_0_cipher_list)
+            else:
+                #logger.info('SSLv3 disabled')
+                scan_entry.sslv3_0 = 'disabled'
+        else:
+            scan_entry.sslv3_0 = 'no_scan'
+
+
+        tls_1_0 = scan_result.scan_result.tls_1_0_cipher_suites
+        if not isinstance(tls_1_0.result, type(None)):
+            if len(tls_1_0.result.accepted_cipher_suites) > 0:
+                tls_1_0_cipher_list = [x.cipher_suite.name for x in tls_1_0.result.accepted_cipher_suites]
+                #logger.info('TLS 1: %s', pformat(tls_1_0_cipher_list))
+                scan_entry.tlsv1_0 = ','.join(tls_1_0_cipher_list)
+            else:
+                #logger.info('TLSv1.0 disabled')
+                scan_entry.tlsv1_0 = 'disabled'
+        else:
+            scan_entry.tlsv1_0 = 'no_scan'
+
+
+        tls_1_1 = scan_result.scan_result.tls_1_1_cipher_suites
+        if not isinstance(tls_1_1.result, type(None)):
+            if len(tls_1_1.result.accepted_cipher_suites) > 0:
+                tls_1_1_cipher_list = [x.cipher_suite.name for x in tls_1_1.result.accepted_cipher_suites]
+                #logger.info('TLS 1.1: %s', pformat(tls_1_1_cipher_list))
+                scan_entry.tlsv1_1 = ','.join(tls_1_1_cipher_list)
+            else:
+                #logger.info('TLSv1.1 disabled')
+                scan_entry.tlsv1_1 = 'disabled'
+        else:
+            scan_entry.tlsv1_1 = 'no_scan'
+
+
+        tls_1_2 = scan_result.scan_result.tls_1_2_cipher_suites
+        if not isinstance(tls_1_2.result, type(None)):
+            if len(tls_1_2.result.accepted_cipher_suites) > 0:
+                tls_1_2_cipher_list = [x.cipher_suite.name for x in tls_1_2.result.accepted_cipher_suites]
+                #logger.info('TLS 1.2: %s', pformat(tls_1_2_cipher_list))
+                scan_entry.tlsv1_2 = ','.join(tls_1_2_cipher_list)
+            else:
+                #logger.info('TLSv1.2 disabled')
+                scan_entry.tlsv1_2 = 'disabled'
+        else:
+            scan_entry.tlsv1_2 = 'no_scan'
+
+
+        tls_1_3 = scan_result.scan_result.tls_1_3_cipher_suites
+        if not isinstance(tls_1_3.result, type(None)):
+            if len(tls_1_3.result.accepted_cipher_suites) > 0:
+                tls_1_3_cipher_list = [x.cipher_suite.name for x in tls_1_3.result.accepted_cipher_suites]
+                #logger.info('TLS 1.3: %s', pformat(tls_1_3_cipher_list))
+                scan_entry.tlsv1_3 = ','.join(tls_1_3_cipher_list)
+            else:
+                #logger.info('TLSv1.3 disabled')
+                scan_entry.tlsv1_3 = 'disabled'
+        else:
+            scan_entry.tlsv1_3 = 'no_scan'
+
+
+        self.session.commit()
 
 
     def scan(self, days):
@@ -304,7 +407,7 @@ class expired_certificate_scanner(object):
     def report(self, days):
         now = datetime.datetime.now()
         now_str = now.strftime('%y%m%d_%H%M%S')
-        output_o = open('{0:s}_results_{1:s}.csv'.format(self.filename, now_str), 'w')
+        output_o = open('results_{0:s}_{1:s}.csv'.format(self.filename, now_str), 'w')
         output_o.write('#host:port,cn,expire,days,issuer\n')
 
         warn_date = now + datetime.timedelta(days=days)
@@ -334,7 +437,7 @@ class expired_certificate_scanner(object):
     def fullreport(self, days):
         now = datetime.datetime.now()
         now_str = now.strftime('%y%m%d_%H%M%S')
-        output_o = open('{0:s}_results_{1:s}.csv'.format(self.filename, now_str), 'w')
+        output_o = open('results_{0:s}_{1:s}.csv'.format(self.filename, now_str), 'w')
         output_o.write('#host:port,cn,fingerprint,selfsigned,expire,days,issuer\n')
 
         query = self.session.query(ScanEntry)\
@@ -383,10 +486,16 @@ class ScanEntry(Base):
     cn            = Column(String(length=100), index=True)
     issuer        = Column(String(length=100), index=True)
     expire        = Column(DateTime, index=True)
-    error         = Column(Text)
     #test_result   = Column(Text)
     test_result   = Column(PickleType)
     fingerprint   = Column(String(length=41))
+    sslv2_0       = Column(Text)
+    sslv3_0       = Column(Text)
+    tlsv1_0       = Column(Text)
+    tlsv1_1       = Column(Text)
+    tlsv1_2       = Column(Text)
+    tlsv1_3       = Column(Text)
+    error         = Column(Text)
 
     __table_args__ = (UniqueConstraint('host', 'port', name='_hostport_uc'),)
 
